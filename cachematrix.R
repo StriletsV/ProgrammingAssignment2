0610fa0f5537f03cd3c3213c649463e060101366
Vova S <vastrelets@gmail.com>
2015-01-23 19:28:16 +0200

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## First function creates a object, list, that contains functions to cache given matrix, get it from cache, cache it's inverse matrix, and get return this inverse matrix
+## First function creates a object, list, that contains functions to cache given matrix, get it from cache,
+## cache it's inverse matrix, and get return this inverse matrix
 ## Second function takes this list, gets matrix and inverses it, then caches inverse matrix with help of setinv
 
-## Creates a special object that can cache its value, give it back, cashe it's inverse value and get it back. This object is a list with metods: set, get, setinv, getinv
+## Creates a special object that can cache its value, give it back, cashe it's inverse value and get it back. 
+## This object is a list with metods: set, get, setinv, getinv
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function(y) {
